d1220e898a05a43b0d4ea07b9c443ce477ebcbd6
kaificial <kaikimto@gmail.com>
2026-01-04 14:28:24 -0500

    Update layout.tsx

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -48,10 +48,10 @@ export default function RootLayout({
             <head>
                 <script
                     dangerouslySetInnerHTML={{
-                        __html: 
+                        __html: `
                             (function() {
                                 // Check localStorage for saved theme preference
-                                const savedTheme = localStorage.getItem('theme');
+                                var savedTheme = localStorage.getItem('theme');
                                 
                                 if (savedTheme) {
                                     // Use saved preference
@@ -65,7 +65,7 @@ export default function RootLayout({
                                     }
                                 }
                             })();
-                        ,
+                        `,
                     }}
                 />
             </head>
