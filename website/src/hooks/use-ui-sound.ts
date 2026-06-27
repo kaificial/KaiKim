f8c22a20e52b32687874b9475f0b0c205ddb48a6
Kai Kim <kaikimto@gmail.com>
2026-06-27 19:49:06 -0400

    feat: add new sound files for hover & click interactions

--- website/src/hooks/use-ui-sound.ts
+++ website/src/hooks/use-ui-sound.ts
@@ -3,10 +3,17 @@
 import useSound from 'use-sound';
 
 export const useUISound = () => {
-    const [playClick] = useSound('/sounds/click.mp3', { volume: 0.35 });
-    const [playHover] = useSound('/sounds/hover.mp3', { volume: 0.06 });
-    const [playOn] = useSound('/sounds/on.mp3', { volume: 0.3 });
-    const [playOff] = useSound('/sounds/off.mp3', { volume: 0.3 });
+    const [playClickRaw, { sound: clickSound }] = useSound('/assets/music/pickup_sound_effect.mp3', { volume: 0.35 });
+    const [playHover] = useSound('/assets/music/drop_sound_effect.mp3', { volume: 0.06 });
+    const [playOn] = useSound('/assets/music/drop_sound_effect.mp3', { volume: 0.3 });
+    const [playOff] = useSound('/assets/music/drop_sound_effect.mp3', { volume: 0.3 });
+
+    const playClick = () => {
+        if (clickSound) {
+            clickSound.seek(2);
+        }
+        playClickRaw();
+    };
 
     return {
         playClick,
